b455c5e6faab740a776ca6c55e623ef15bc39b3d
unknown <yunjuan86@gmail.com>
2024-05-23 23:00:43 +0800

    modified join room feature

--- frontend/src/components/Room.js
+++ frontend/src/components/Room.js
@@ -7,7 +7,7 @@ const Room = () => {
     const [guestCanPause, setGuestCanPause] = useState(false);
     const [isHost, setIsHost] = useState(false);
     const location = useLocation();
-    
+    const [success, setSuccess] = useState(true);
     const match = matchPath(
         { path: "/room/:param" },
         location.pathname,
@@ -19,26 +19,33 @@ const Room = () => {
         const getRoomDetails = async () => {
             try {
                 const response = await fetch("/api/get-room?code=" + roomCode);
+                if(!response.ok){
+                    console.error("Error fetching room details:", error);
+                    setSuccess(false);
+                }
                 const data = await response.json();
                 setVotesToSkip(data.votes_to_skip);
                 setGuestCanPause(data.guest_can_pause);
                 setIsHost(data.is_host);
             } catch (error) {
                 console.error("Error fetching room details:", error);
+                setSuccess(false);
             }
         };
 
         getRoomDetails();
     }, [roomCode]);
-
+if(success){
     return (
         <div>
             <h3>Room: {roomCode}</h3>
             <p>Votes: {votesToSkip}</p>
-            <p>Guest Can Pause: {guestCanPause.toString()}</p>
-            <p>Host: {isHost.toString()}</p>
+            <p>Guest Can Pause: {guestCanPause}</p>
+            <p>Host: {isHost}</p>
         </div>
     );
+}
+return (<h1>Room does not exist!</h1>)
 };
 
 export default Room;
